86556066f758bc16ee8e8dbe6545b58d85ca64f0
Anton <aimozg@ya.ru>
2018-11-14 19:27:02 +0600

    reorganization (java -> kotlin, toPos -> positionAt in satpos.kt)

---
+++ src/main/kotlin/gnss/calc/nav_t.kt
@@ -0,0 +1,144 @@
+package gnss.calc
+
+import java.util.*
+
+class nav_t {
+	val eph = ArrayList<eph_t>()         /* GPS/QZS/GAL ephemeris */
+	//    public final ArrayList<geph_t> geph;       /* GLONASS ephemeris */
+	//    public seph_t seph;       /* SBAS ephemeris */
+	//    public peph_t peph;       /* precise ephemeris */
+	//    public pclk_t pclk;       /* precise clock */
+	//    public alm_t alm;         /* almanac data */
+	//    public tec_t tec;         /* tec grid data */
+	//    public stec_t stec;       /* stec grid data */
+	//    public erp_t  erp;         /* earth rotation parameters */
+	val utc_gps = DoubleArray(4)  /* GPS delta-UTC parameters {A0,A1,T,W} */
+	val utc_glo = DoubleArray(4)  /* GLONASS UTC GPS time parameters */
+	val utc_gal = DoubleArray(4)  /* Galileo UTC GPS time parameters */
+	val utc_qzs = DoubleArray(4)  /* QZS UTC GPS time parameters */
+	val utc_cmp = DoubleArray(4)  /* BeiDou UTC parameters */
+	val utc_sbs = DoubleArray(4)  /* SBAS UTC parameters */
+	val ion_gps = DoubleArray(8)  /* GPS iono model parameters {a0,a1,a2,a3,b0,b1,b2,b3} */
+	val ion_gal = DoubleArray(4)  /* Galileo iono model parameters {ai0,ai1,ai2,0} */
+	val ion_qzs = DoubleArray(8)  /* QZSS iono model parameters {a0,a1,a2,a3,b0,b1,b2,b3} */
+	val ion_cmp = DoubleArray(8)  /* BeiDou iono model parameters {a0,a1,a2,a3,b0,b1,b2,b3} */
+	var leaps: Int = 0          /* leap seconds (s) */
+	//   public final double lam[MAXSAT][NFREQ]; /* carrier wave lengths (m) */
+	//   public final double cbias[MAXSAT][3];   /* code bias (0:p1-p2,1:p1-c1,2:p2-c2) (m) */
+	//   public final double wlbias[MAXSAT];     /* wide-lane bias (cycle) */
+	//   public final double[] glo_cpbias = new double[4];    /* glonass code-phase bias {1C,1P,2C,2P} (m) */
+	//   public char glo_fcn; /* glonass frequency channel number + 8 */
+	//   public final pcv_t pcvs[MAXSAT]; /* satellite antenna pcv */
+	//   public sbssat_t sbssat;    /* SBAS satellite corrections */
+	//   public final sbsion_t sbsion[MAXBAND+1]; /* SBAS ionosphere corrections */
+	//   public final dgps_t dgps[MAXSAT]; /* DGPS corrections */
+	//   public final ssr_t ssr[MAXSAT];  /* SSR corrections */
+	//   public final lexeph_t lexeph[MAXSAT]; /* LEX ephemeris */
+	//   public lexion_t lexion;    /* LEX ionosphere correction */
+	internal fun add_eph(eph: eph_t): Boolean {
+		return this.eph.add(eph)
+	}
+	//    boolean add_geph(geph_t geph){
+	//        return this.geph.add(geph);
+	//    }
+
+}
+
+class eph_t(ver: Double, var sat: Int, var toc: Date, data: ArrayList<Double>) {
+    private val sys = satsys(sat).sys
+
+    var f0 = data[0]
+    var f1 = data[1]
+    var f2 = data[2]
+
+    var A = Math.pow(data[10], 2.0)
+    var e = data[8]
+    var i0 = data[15]
+    var OMG0 = data[13]
+    var omg = data[17]
+    var M0 = data[6]
+    var deln = data[5]
+    var OMGd = data[18]
+    var idot = data[19]
+
+    var crc = data[16]
+    var crs = data[4]
+    var cuc = data[7]
+    var cus = data[9]
+    var cic = data[12]
+    var cis = data[14]
+
+    var tgd = when (sys) {
+        SYS_GPS, SYS_QZS -> {
+            arrayOf(data[25], null, null, null)
+        }
+        SYS_GAL, SYS_CMP -> {
+            arrayOf(data[25], data[26], null, null)
+        }
+        else -> arrayOfNulls(4)
+    }
+
+    var Adot = 0.0
+    var ndot = 0.0
+
+    var iode = data[3].toInt()
+    var iodc = when (sys) {
+        SYS_GPS, SYS_QZS -> data[26].toInt()
+        SYS_CMP -> data[28].toInt()
+        else -> 0
+    }
+    var toes = data[11]
+    var week = data[21].toInt()
+    var toe: Date = when (sys) {
+        SYS_GPS, SYS_QZS, SYS_GAL -> adjweek(gps2time(week, toes), toc)
+        else -> gps2time(week, toes) //fixme bdt2gpst
+    }
+    lateinit var ttr: Date
+    var sva = 0
+    var svh = when (sys) {
+        SYS_GPS, SYS_QZS, SYS_GAL -> data[24].toInt()
+        else -> 0
+    }
+    var code = when (sys) {
+        SYS_GPS, SYS_QZS, SYS_GAL -> data[20].toInt()
+        else -> 0
+    }
+    var flag = when (sys) {
+        SYS_GPS, SYS_QZS -> data[22].toInt()
+        else -> 0
+    }
+
+    var fit = when (sys) {
+        SYS_GPS, SYS_QZS -> data[28]
+        else -> 0.0
+    }
+
+}
+
+internal fun timediff(time: Date, toe: Date): Double {
+	return ((time.time - toe.time) / 1000).toDouble()
+}
+
+internal fun adjweek(t: Date, t0: Date): Date {
+	val tt = timediff(t, t0)
+	if (tt < -302400.0) return timeadd(t, 604800.0)
+	if (tt > 302400.0) return timeadd(t, -604800.0)
+	return t
+}
+
+class geph_t {
+	var sat: Int = 0            /* satellite number */
+	var iode: Int = 0           /* IODE (0-6 bit of tb field) */
+	var frq: Int = 0            /* satellite frequency number */
+	var svh: Int = 0
+	var sva: Int = 0
+	var age: Int = 0    /* satellite health, accuracy, age of operation */
+	var toe: Date = Date()        /* epoch of epherides (gpst) */
+	var tof: Date = Date()        /* message frame time (gpst) */
+	val pos = DoubleArray(3)      /* satellite position (ecef) (m) */
+	val vel = DoubleArray(3)      /* satellite velocity (ecef) (m/s) */
+	val acc = DoubleArray(3)      /* satellite acceleration (ecef) (m/s^2) */
+	var taun: Double = 0.0
+	var gamn: Double = 0.0   /* SV clock bias (s)/relative freq bias */
+	var dtaun: Double = 0.0       /* delay between L1 and L2 (s) */
+}
---
+++ src/main/kotlin/gnss/calc/satpos.kt
@@ -0,0 +1,100 @@
+package gnss.calc
+
+import javafx.geometry.Point3D
+import java.util.*
+
+/*
+ * Created by aimozg on 14.11.2018.
+ * Confidential unless published on GitHub
+ */
+
+private fun var_uraeph(ura: Int): Double {
+	val ura_value = arrayOf(
+			2.4, 3.4, 4.85, 6.85, 9.65, 13.65, 24.0, 48.0, 96.0, 192.0, 384.0, 768.0, 1536.0,
+			3072.0, 6144.0
+	)
+	if ((ura < 0 || 15 < ura)) {
+		return Math.pow(6144.0, 2.0)
+	}
+	return ura_value[ura]
+}
+
+fun eph_t.positionAt(time: Date): Pos {
+    if (A <= 0.0) {
+        return Pos(Point3D.ZERO, 0.0, 0.0)
+    }
+    var tk = timediff(time, toe)
+    val satinfo = satsys(sat)
+    val sys = satinfo.sys
+    val prn = satinfo.prn
+    val mu = when (sys) {
+        SYS_GAL -> MU_GAL
+        SYS_CMP -> MU_CMP
+        else -> MU_GPS
+    }
+    val omge = when (sys) {
+        SYS_GAL -> OMGE_GAL
+        SYS_CMP -> OMGE_CMP
+        else -> OMGE
+    }
+    val M = M0 + (Math.sqrt(mu / (A * A * A)) + deln) * tk
+    var n = 0
+    var E = M
+    var Ek = 0.0
+    while (Math.abs(E - Ek) > RTOL_KEPLER && n < MAX_ITER_KEPLER) {
+        Ek = E;
+        E -= (E - e * Math.sin(E) - M) / (1.0 - e * Math.cos(E))
+        n++
+    }
+    if (n >= MAX_ITER_KEPLER) {
+        throw Exception("kepler iteration overflow sat=$sat")
+    }
+    val sinE = Math.sin(E)
+    val cosE = Math.cos(E)
+
+    var u = Math.atan2(Math.sqrt(1.0 - e * e) * sinE, cosE - e) + omg
+    var r = A * (1.0 - e * cosE)
+    var i = i0 + idot * tk
+    val sin2u = Math.sin(2.0 * u)
+    val cos2u = Math.cos(2.0 * u)
+
+    u += cus * sin2u + cuc * cos2u
+    r += crs * sin2u + crc * cos2u
+    i += cis * sin2u + cic * cos2u
+
+    val x = r * Math.cos(u)
+    val y = r * Math.sin(u)
+
+    val cosi = Math.cos(i)
+
+    var point3D: Point3D
+    if (sys == SYS_CMP && prn <= 5) {
+        val O = OMG0 + OMGd * tk - omge * toes
+        val sinO = Math.sin(O)
+        val cosO = Math.cos(O)
+        val xg = x * cosO - y * cosi * sinO
+        val yg = x * sinO + y * cosi * cosO
+        val zg = y * Math.sin(i)
+        val sino = Math.sin(omge * tk)
+        val coso = Math.cos(omge * tk)
+        point3D = Point3D(
+		        xg * coso + yg * sino * COS_5 + zg * sino * SIN_5,
+		        -xg * sino + yg * coso * COS_5 + zg * coso * SIN_5,
+		        -yg * SIN_5 + zg * COS_5
+        )
+    } else {
+        val O = OMG0 + (OMGd - omge) * tk - omge * toes
+        val sinO = Math.sin(O)
+        val cosO = Math.cos(O)
+        point3D = Point3D(
+		        x * cosO - y * cosi * sinO,
+		        x * sinO + y * cosi * cosO,
+		        y * Math.sin(i)
+        )
+    }
+    tk = timediff(time, toc)
+    var dts = f0 + f1 * tk + f2 * tk * tk
+    dts -= 2.0 * Math.sqrt(mu * A) * e * sinE / Math.pow(CLIGHT, 2.0)
+    val variance = var_uraeph(sva)
+    return Pos(point3D, dts, variance)
+}
