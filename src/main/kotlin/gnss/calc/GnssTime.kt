c00418c4772fee72827db93ed6a1ab6fa81bf708
Anton <aimozg@ya.ru>
2018-11-15 20:06:28 +0600

    genprc and GnssTime (gtime_t)

---
+++ src/main/kotlin/gnss/calc/GnssTime.kt
@@ -0,0 +1,82 @@
+package gnss.calc
+
+import java.util.*
+
+/*
+ * Created by aimozg on 15.11.2018.
+ * Confidential unless published on GitHub
+ */
+class GnssTime(
+		/** time (s) expressed by standard time_t */
+		time: Long,
+		/** fraction of second under 1 s */
+		sec: Double): Comparable<GnssTime> {
+	constructor(date: Date): this(date.time/1000, (date.time % 1000)/1000.0)
+
+	/** time (s) expressed by standard time_t */
+	val time:Long
+	/** fraction of second under 1 s */
+	val sec:Double
+	init {
+		if (sec >= 0.0 && sec < 1.0) {
+			this.time = time
+			this.sec = sec
+		} else {
+			val i = Math.floor(sec)
+			this.time = time + i.toLong()
+			this.sec = sec - i
+			/* case A:
+			   sec = -2.4
+			   i = -3.0
+			   time = time - 3
+			   sec = -2.4 - (-3.0) = 0.6
+
+			   case B:
+			   sec = 2.6
+			   i = 2.0
+			   time = time + 2
+			   sec = 2.6 - 2.0 = 0.6
+			 */
+		}
+	}
+
+	fun toDate():Date {
+		return Date(time*1000L + sec.toLong())
+	}
+
+	operator fun minus(other: GnssTime):Double {
+		return (this.time - other.time).toDouble() + (this.sec - other.sec)
+	}
+
+	override operator fun compareTo(other: GnssTime) = when {
+		this.time < other.time -> -1
+		this.time > other.time -> +1
+		this.sec < other.sec -> -1
+		this.sec > other.sec -> +1
+		else -> 0
+	}
+
+	override fun equals(other: Any?): Boolean {
+		if (this === other) return true
+		if (javaClass != other?.javaClass) return false
+
+		other as GnssTime
+
+		if (time != other.time) return false
+		if (sec != other.sec) return false
+
+		return true
+	}
+
+	override fun hashCode(): Int {
+		var result = time.hashCode()
+		result = 31 * result + sec.hashCode()
+		return result
+	}
+
+	override fun toString(): String {
+		return "{time:$time, sec:$sec}"
+	}
+
+
+}
---
+++ src/main/kotlin/gnss/calc/genprc.kt
@@ -0,0 +1,55 @@
+package gnss.calc
+
+import kotlin.math.pow
+
+/*
+ * Created by aimozg on 15.11.2018.
+ * Confidential unless published on GitHub
+ */
+
+/**
+ * Calculates pseudorange correction - the difference between computed geometric range (cr) and adjusted pseudorange (adjr):
+ * cr - distance between the base and the satellite at the time of transmission (ttr = time - range / c) corrected for the Earth's rotation,
+ * adjr = range + Δtsv(ttr) * c - Tgd * c,
+ * @param time - time of the pseudorange measurement,
+ * @param range - pseudorange (m),
+ * @param base - base coordinates (ecef) (m) (double[3]),
+ * @param eph - ephemeris parameters for the correction calculation.
+ * @return the calculated pseudorange correction.
+ */
+fun genprc(time: GnssTime, range: Double, base: DoubleArray, eph: eph_t): Double {
+	val dt = -range / CLIGHT
+	val time1 = GnssTime(time.time, time.sec + dt)
+	val pos = eph.positionAt(time1.toDate())
+	val x = pos.point3D.x
+	val y = pos.point3D.y
+	val z = pos.point3D.z
+	val adjr = range + pos.dts * CLIGHT - (eph.tgd[0] ?: 0.0) * CLIGHT
+	val cr = ((base[0] - x).pow(2) + (base[1] - y).pow(2) + (base[2] - z).pow(2)).pow(0.5)
+	val dr = OMGE / CLIGHT * (x * base[1] - y * base[0])
+	return cr - adjr + dr
+}
+
+/*
+double gengprc(gtime_t time,double range,const double * base,const geph_t * eph)
+{
+ double rs[3];
+ double adjr;
+ double cr;
+ double dr;
+ double dts;
+ double var;
+ time.sec-=range/CLIGHT;
+ if(time.sec<0)
+ {
+  --time.time;
+  time.sec=1.0+time.sec;
+ }
+ geph2pos(time,eph,rs,&dts,&var);
+ adjr=range+dts*CLIGHT;
+ cr=sqrt(pow(base[0]-rs[0],2)+pow(base[1]-rs[1],2)+pow(base[2]-rs[2],2));
+ dr=OMGE/CLIGHT*(rs[0]*base[1]-rs[1]*base[0]);
+ return cr-adjr+dr;
+}
+
+ */
